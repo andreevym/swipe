5c35cc240e4754f18c92edf826c76696cc4824e8
Yury Andreev <andreevym@gmail.com>
2016-04-10 18:15:46 +0300

    Перелистывание страниц жестами влево вправо

--- app/src/main/java/ru/android_studio/swipe/MainActivity.java
+++ app/src/main/java/ru/android_studio/swipe/MainActivity.java
@@ -1,13 +1,105 @@
 package ru.android_studio.swipe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        if (savedInstanceState == null) {
+            // при первом запуске программы
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            OneFragment oneFragment = new OneFragment();
+            fragmentTransaction.add(R.id.fragment, oneFragment, "One");
+            fragmentTransaction.commit();
+        }
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                    }
+                    result = true;
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
+                    } else {
+                        onSwipeTop();
+                    }
+                }
+                result = true;
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    public void onSwipeRight() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right);
+
+        ThreeFragment twoFragment = new ThreeFragment();
+        transaction.replace(R.id.fragment, twoFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void onSwipeLeft() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left);
+
+        TwoFragment twoFragment = new TwoFragment();
+        transaction.replace(R.id.fragment, twoFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void onSwipeTop() {
+    }
+
+    public void onSwipeBottom() {
+    }
+
 }
